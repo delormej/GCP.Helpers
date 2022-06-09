8a6ba7e4e5ec731bdf69edd42b6441dd24e871a0
Jason De Lorme <delormej@gmail.com>
2022-06-09 07:19:45 -0400

    Fixing dir

--- publish.sh
+++ publish.sh
@@ -4,9 +4,6 @@ echo 'Publishing version $version' to nuget.org...
 
 export nugetkey=`cat nuget-key` 
 
-pushd
-cd bin/debug/
-dotnet nuget push GcpHelpers.$version.nupkg --api-key $nugetkey --source https://api.nuget.org/v3/index.json
-popd
+dotnet nuget push ./bin/Debug/GcpHelpers.$version.nupkg --api-key $nugetkey --source https://api.nuget.org/v3/index.json
 
 echo 'Done'
